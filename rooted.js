611a335af401aa17057fec69d421bc4f71d09acd
Izzy Donoghue <izzydonoghue@Izzys-MacBook-Pro-4.local>
2025-01-12 19:55:22 -0600

    debugging done

--- rooted.js
+++ rooted.js
@@ -1,4 +1,3 @@
-// DUMMY ROOTED FILE USE ONLY FOR DEBUGGING
 const puppeteer = require('puppeteer');
 
 // parse command line arguments
@@ -11,6 +10,9 @@ function getArgValue(flag) {
     return index > -1 ? args[index + 1] : null;
 }
 // extract values
+// HARDCODE
+//deliveryDate = "2025-07-08"
+//flowerNames = ["PAEONIA", "STOCK", "SNAPDRAGON", "SALAL", "DELPHINIUM", "ROSE", "CARNATION", "LISIANTHUS", "SCABIOSA", "MUMS", "RANUNCULUS", "ANEMONE", "EUCALYPTUS", "RUSCUS"];
 deliveryDate = getArgValue('--deliveryDate') || '';
 flowerNames = getArgValue('--flowerNames') ? getArgValue('--flowerNames').split(',') : [
     "STOCK", "SNAPDRAGON", "SALAL", "DELPHINIUM", "ROSE", "CARNATION", "LISIANTHUS", "SCABIOSA", "MUMS", "RANUNCULUS", "ANEMONE", "EUCALYPTUS", "RUSCUS"
@@ -22,7 +24,6 @@ let numPages = 0;
     let browser = null;
 
     try {
-
         browser = await puppeteer.launch(); // launches puppeteer browser instance
         const page = await browser.newPage(); // opens new browser tab
         //console.log("loaded browser")
@@ -30,76 +31,44 @@ let numPages = 0;
         // print browser console messages
         // page.on('console', async msg => {
         //     const args = await Promise.all(msg.args().map(arg => arg.jsonValue()));
-        //     if (args.length > 0 && args[0].includes("console:")) {
+        //     if (args.length > 0 && typeof args[0] === 'string' && args[0].includes("console:")) {
         //         console.log(`${args}`);
         //     }
         // });
 
-        // login to kennicott
-        const url = "https://www.rootedfarmers.com/account/sign-in";
+        // login to rooted farmers
+        const url = "https://www.rootedfarmers.com/shop";
         const username = "shop@uniflowershop.com";
         const password = "RRShappydayz5!";
 
-        //await page.goto(url); // load home page
+        await page.goto(url); // load page
 
-        // FOR FRONTEND DEBUG
-        flowers.push({
-            flowerName: 'ROSE',
-            flowerImage: 'https://cdn.pixabay.com/photo/2023/11/06/09/43/lotus-8369252_1280.jpg',
-            prices: '$12.00/BU, $1.20/ST',
-            stemPrice: '2.00', 
-            color: 'White',
-            height: '20 cm',
-            stemsPer: '120',
-            seller: "Rooted Farmers",
-            farm: 'LIVE & LOCAL',
-            available: '10 BU',
-            delivery: '10/20/2024',
-        });
+        // automate login
+        // username
+        await page.waitForSelector('#login_email');
+        await page.type('#login_email', username);
+        // password
+        await page.waitForSelector('#login_password');
+        await page.type('#login_password', password);
+        // submit button
+        await page.waitForSelector('button[type="submit"]');
+        await page.click('button[type="submit"]');
+        //console.log("clicked sign in button");
+        await page.waitForNavigation();
+        //console.log("login success")
 
-        // FOR FRONTEND DEBUG
-        flowers.push({
-            flowerName: 'CARNATION',
-            flowerImage: 'https://cdn.pixabay.com/photo/2023/11/06/09/43/lotus-8369252_1280.jpg',
-            prices: '$12.00/BU, $1.50/ST',
-            stemPrice: '1.00', 
-            color: 'Red',
-            height: '10 cm',
-            stemsPer: '100',
-            seller: "Rooted Farmers",
-            farm: 'Dutch Direct',
-            available: '20 BU',
-            delivery: '10/04/2024',
-        });
-                // FOR FRONTEND DEBUG
-        flowers.push({
-            flowerName: 'SALAL',
-            flowerImage: 'https://cdn.pixabay.com/photo/2023/11/06/09/43/lotus-8369252_1280.jpg',
-            prices: '$20.00/BU, $1.60/ST',
-            stemPrice: '6.00', 
-            color: 'Pink',
-            height: '20 cm',
-            stemsPer: '120',
-            seller: "Rooted Farmers",
-            farm: 'LIVE & LOCAL',
-            available: '0 BU',
-            delivery: '10/02/2024',
-        });
+        // convert delivery date from frontend into delivery date wanted
+        // input - YYYY-MM-DD
+        // output - MM/DD/YYYY
+        const [inputYear, inputMonth, inputDay] = deliveryDate.split('-').map(Number);
+        let formattedInputDate = new Date(inputYear, inputMonth - 1, inputDay);
+        const inputMonthString = (formattedInputDate.getMonth() + 1).toString().padStart(2, '0');
+        const inputDayString = formattedInputDate.getDate().toString().padStart(2, '0');
+        const inputYearString = formattedInputDate.getFullYear();
+        deliveryDate = `${inputMonthString}/${inputDayString}/${inputYearString}`;
 
-        // FOR FRONTEND DEBUG
-        flowers.push({
-            flowerName: 'MUMS',
-            flowerImage: 'https://cdn.pixabay.com/photo/2023/11/06/09/43/lotus-8369252_1280.jpg',
-            prices: '$12.00/BU, $1.00/ST',
-            stemPrice: '2.50', 
-            color: 'Dark Red',
-            height: '1 cm',
-            stemsPer: '10',
-            seller: "Rooted Farmers",
-            farm: 'Dutch Direct',
-            available: '75 BU',
-            delivery: '10/01/2024',
-        });
+        // scrape flower data
+        flowers = await scrapeData(page, flowers, deliveryDate);
 
     } catch (err) {
         console.error("error during login or page load:", err);
@@ -108,8 +77,224 @@ let numPages = 0;
             await browser.close();
             //console.log("closed browser");
         }
-        // console.log("scraped all data");
+        //console.log("scraped all data");
         console.log(JSON.stringify(flowers));
     }
 })();
 
+function formatDeliveryDate(deliveryDate) {
+    // split deliveryDate string into an array [MM, DD, YYYY]
+    const [month, day, year] = deliveryDate.split('/');
+
+    // return the formatted date as MM/DD without leading zeros
+    return `${parseInt(month, 10)}/${parseInt(day, 10)}`;
+}
+
+async function scrapeData(page, flowers, deliveryDate) {
+    let hasNextPage = true;
+
+    while (hasNextPage) {
+        try {
+            await page.waitForSelector('.ant-spin-container'); // wait for the product list to load
+            //console.log("table loaded");
+
+            const newFlowers = await extractFlowerData(page, flowerNames, deliveryDate);
+            //console.log("scraped page", numPages);
+            flowers = flowers.concat(newFlowers);
+            //console.log("added flowers");
+
+            // check for next page button
+            let nextPageButton;
+            try {
+                nextPageButton = await page.$('.ant-pagination-next button.ant-pagination-item-link');
+                if (!nextPageButton) {
+                    throw new Error("Next button not found");
+                }
+            } catch (err) {
+                console.error("next button does not exist:", err.message);
+                hasNextPage = false; // exit loop
+                continue;
+            }
+
+            const isDisabled = await page.$eval('.ant-pagination-next button.ant-pagination-item-link', el => el.hasAttribute('disabled'));
+            if (!isDisabled) {
+                numPages += 1;
+                await nextPageButton.click();
+                //console.log("clicked next page");
+                // wait for data reload
+                await page.waitForFunction(
+                    () => !document.querySelector('.ant-list-loading'),
+                    { timeout: 10000 }
+                );
+                //console.log("next page", numPages)
+            } else {
+                hasNextPage = false;
+            }
+        } catch (err) {
+            console.error("error during pagination or scraping:", err);
+            hasNextPage = false;
+        }
+    }
+    return flowers;
+}
+
+async function extractFlowerData(page, flowerNames, deliveryDate) {
+    // format date
+    date = formatDeliveryDate(deliveryDate);
+    //console.log("entered extractFlowerData")
+    try {
+        await page.waitForSelector('ul.ant-list-items');
+        //console.log("products loaded")
+
+        return await page.evaluate(async (flowerNames, date, deliveryDate) => {
+            const items = document.querySelectorAll('ul.ant-list-items .ant-list-item');
+            //console.log("console: items selected")
+    
+            let flowersData = [];
+            const promises = [];
+
+            items.forEach(item => {
+                // extracts flower name in all caps
+                const flowerNameElement = item.querySelector('div.ant-space-item span.ant-typography b');
+                let flowerName = '';
+                if (flowerNameElement) {
+                    flowerName = flowerNameElement.textContent.trim().toUpperCase();
+                }
+                //console.log("console: name ", flowerName)
+
+                // check if current name matches name from flowerNames list
+                const containsFlowerName = flowerNames.some(name => flowerName.includes(name));
+
+                // scrapes matching available flowers
+                if (containsFlowerName) {
+                    //console.log("console: name ", flowerName)
+
+                    // array of item delivery dates
+                    const dateFound = false;
+                    // click select button to render div
+                    const selectButton = item.querySelector('button.ant-btn.ant-btn-primary.ant-btn-lg');
+                    if (selectButton) {
+                        selectButton.click();
+                        //console.log("console: select button clicked");
+                    }
+                    // New div dynamically loaded
+                    const promise = new Promise((resolve, reject) => {
+                        const observer = new MutationObserver((mutationsList) => {
+                            for (const mutation of mutationsList) {
+                                if (mutation.type === 'childList') {
+                                    const newDiv = document.querySelector('.ant-popover');
+                                    if (newDiv) {
+                                        // Extract date strings
+                                        const dateElements = newDiv.querySelectorAll('button span.ant-typography');
+                                        const dateStrings = Array.from(dateElements).map(element => element.textContent.trim());
+                                        //console.log("console: extracted dates:", dateStrings.join(' '));
+
+                                        // Check if any extracted date matches the passed 'date'
+                                        if (dateStrings.includes(date)) {
+                                            //console.log(`console: delivery date ${date} matched`);
+                                            resolve(dateStrings);  // Resolve the promise with the extracted dates
+                                            observer.disconnect();  // Stop observing after extraction
+                                        }
+                                    }
+                                }
+                            }
+                        });
+
+                        observer.observe(document.body, { childList: true, subtree: true });
+                        setTimeout(() => {
+                            //console.log("console: timeout reached, no date match found");
+                            observer.disconnect();  // Stop observing
+                            resolve([]);  // Resolve with an empty array to indicate no match
+                        }, 5000);  // Adjust timeout as needed
+                    });
+                    promises.push(promise);
+
+                    // scrape price
+                    const priceElement = item.querySelector('div.ant-space-item span.ant-typography[aria-label="Current value"]');
+                    const prices = priceElement ? priceElement.textContent.trim() : '';
+                    //console.log("console: price ", prices)
+
+                    // scrape color
+                    const colorElement = item.querySelector('div.ant-space-item span.ant-badge-status-text span.ant-typography');
+                    const color = colorElement ? colorElement.textContent.trim() : '';
+                    //console.log("console: color ", color)
+
+                    // scrape vendor/farm
+                    const farmElement = item.querySelector('div.ant-space-item span.ant-typography a');
+                    const farm = farmElement ? farmElement.textContent.trim() : '';
+                    //console.log("console: farm", farm)
+
+                    // scrape stems per
+                    const stemsPerElement = item.querySelector('div.ant-space-item span.ant-typography');
+                    let stemsPer = '';
+                    if (stemsPerElement) {
+                        const textContent = stemsPerElement.textContent.trim();
+                        //console.log("console: stemsPerElement textContent", textContent);
+                        const match = textContent.match(/\((\d+)\s+stems\s+\/\s+bunch\)/i);
+                        if (match) {
+                            stemsPer = match[1] + ' ST/BU';
+                        }
+                    }
+                    //console.log("console: stemsPerBunch ", stemsPer);
+
+                    // scrape stock available
+                    const availabilityElement = item.querySelector('div.ant-space-item span.ant-typography[style="font-size: 14px;"]');
+                    let available = '';
+                    if (availabilityElement) {
+                        const availabilityText = availabilityElement.textContent.trim();
+                        //console.log("console: availabilityElement textContent", availabilityText);
+                        const match = availabilityText.match(/(\d+)\s+bunches in stock/i);
+                        available = match ? `${match[1]} BU` : '';
+                    }
+                    //console.log("console: availability ", available)
+
+                    // delivery date passed in
+                    const delivery = deliveryDate;
+
+                    // price per one stem
+                    const stemPriceElement = item.querySelector('div.ant-space-item span.ant-typography[aria-label="Current value"]');
+                    const stemPrice = stemPriceElement ? stemPriceElement.textContent.trim().replace(/[^\d.]/g, '') : '';
+                    //console.log("console: stemPrice", stemPrice);
+
+                    // scrape height
+                    const heightElement = item.querySelector('div.ant-space-item span.ant-typography[title="Stem length"]');
+                    const height = heightElement ? heightElement.textContent.trim() : '';
+                    //console.log("console: height", height);
+
+                    // scrape image url
+                    const imageElement = item.querySelector('div.ant-image img.ant-image-img');
+                    const flowerImage = imageElement ? imageElement.src : '';
+                    //console.log("console: flowerImage", flowerImage);
+
+                    
+                    flowersData.push({
+                        flowerName,
+                        flowerImage,
+                        prices,
+                        stemPrice,
+                        color,
+                        height,
+                        stemsPer,
+                        seller: "Rooted Farmers",
+                        farm,
+                        available,
+                        delivery
+                    });
+                }
+            });
+
+            // check delivery date matches
+            const resolvedDates = await Promise.all(promises);
+            for (let i = resolvedDates.length - 1; i >= 0; i--) {
+                if (!resolvedDates[i].includes(date)) {
+                    flowersData.splice(i, 1);  // Remove flower data if no matching date found
+                }
+            }
+
+            return flowersData;
+        }, flowerNames, date, deliveryDate);
+    } catch (err) {
+        console.error("error during data extraction:", err);
+        return [];
+    }
+}
